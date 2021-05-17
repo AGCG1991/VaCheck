226201b1ffb57370cbf3005b8c4a936c62aaa8b8
JaviML15 <javiermlara55@hotmail.es>
2021-05-17 19:35:29 +0200

    Clase cita medica

---
+++ Ficheros/hospital/src/main/java/trabajo/vacheck/hospital/CitaMedica.java
@@ -0,0 +1,70 @@
+package trabajo.vacheck.hospital;
+
+import java.util.Date;
+import java.util.Objects;
+
+@SuppressWarnings("ALL")
+public class CitaMedica {
+
+    private String IdCita; //Identificador unico de la cita
+    private Date fechaCita;
+    private String hospitalAsignado;
+
+    //Métodos getter y setter
+
+    public String getIdCita() {
+        return IdCita;
+    }
+
+    public void setIdCita(String idCita) {
+        IdCita = idCita;
+    }
+
+    public Date getFechaCita() {
+        return fechaCita;
+    }
+
+    public void setFechaCita(Date fechaCita) {
+        this.fechaCita = fechaCita;
+    }
+
+    //Equals y hash code que tiene en cuenta el Id de la cita para comparar
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CitaMedica that = (CitaMedica) o;
+        return Objects.equals(IdCita, that.IdCita);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(IdCita);
+    }
+
+    //Método toString para mostrar los datos de la cita
+
+
+    @Override
+    public String toString() {
+        return "CitaMedica{" +
+                "IdCita='" + IdCita + '\'' +
+                ", fechaCita=" + fechaCita +
+                ", hospitalAsignado='" + hospitalAsignado + '\'' +
+                '}';
+    }
+
+    public void AltaCita(int anyo, int mes, int dia, int hr, int min){
+        fechaCita = new Date(anyo, mes, dia, hr, min);
+    }
+
+    public void BajaCita(){
+        fechaCita = null;
+    }
+
+    public void ActualizarCita(Date f){
+        fechaCita = f;
+    }
+
+}
